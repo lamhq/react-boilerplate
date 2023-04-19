799191c487fb19b92c392e8a2cd3cf21306bbced
Lam <daibanglam@gmail.com>
2023-04-19 22:30:20 +0700

    feat: setup eslint and debug with vscode

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+
+  // ignore files in root directory
+  ignorePatterns: ['/*.*'],
+
+  // additional rules
+  rules: {
+    // Vite automatically import React for us, so no need this rule
+    'react/react-in-jsx-scope': 'off',
+
+    // allow importing asset with Vite
+    'import/no-absolute-path': 'off',
+  },
+};
