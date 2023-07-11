d866acb2fd8fd34fb1e82ec23dc7c74e6bb79942
Lam <daibanglam@gmail.com>
2023-07-11 09:53:49 +0700

    feat: add MUI lib

--- src/common/templates/App/index.tsx
+++ src/common/templates/App/index.tsx
@@ -1,11 +1,17 @@
 import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
 import { ServiceProvider, services } from 'src/services';
 import { router } from 'src/router';
+import { theme } from 'src/theme';
+import { CssBaseline } from '@mui/material';
 
 export function App() {
   return (
     <ServiceProvider services={services}>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </ServiceProvider>
   );
 }
