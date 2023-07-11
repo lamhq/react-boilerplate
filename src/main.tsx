8b206869c2334710568aa5b2aa695d2bb2e9ccf2
Lam <daibanglam@gmail.com>
2023-07-11 10:19:04 +0700

    chore(main.tsx): test commitizen

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './common/templates/App';
 
+console.log('debug code');
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
