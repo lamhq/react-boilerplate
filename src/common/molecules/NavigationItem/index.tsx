e226b061b517e5e689a0883d119db261aff826b5
Lam <daibanglam@gmail.com>
2023-07-12 16:23:33 +0700

    feat: implement active link style

--- src/common/molecules/NavigationItem/index.tsx
+++ src/common/molecules/NavigationItem/index.tsx
@@ -2,6 +2,7 @@ import { NavLinkProps, NavLink } from 'react-router-dom';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import { styled } from '@mui/material/styles';
 
 export interface NavigationItemProps {
   to: NavLinkProps['to'];
@@ -9,11 +10,20 @@ export interface NavigationItemProps {
   label: string;
 }
 
+const MyNavLink = styled(NavLink)({
+  textDecoration: 'none',
+  color: 'inherit',
+});
+
 export default function NavigationItem({ to, icon, label }: NavigationItemProps) {
   return (
-    <ListItemButton component={NavLink} to={to}>
-      <ListItemIcon sx={{ minWidth: 36 }}>{icon}</ListItemIcon>
-      <ListItemText primary={label} />
-    </ListItemButton>
+    <MyNavLink to={to}>
+      {({ isActive }) => (
+        <ListItemButton selected={isActive}>
+          <ListItemIcon sx={{ minWidth: 36 }}>{icon}</ListItemIcon>
+          <ListItemText primary={label} />
+        </ListItemButton>
+      )}
+    </MyNavLink>
   );
 }
