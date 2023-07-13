1235431c78bc30e95811f552d9b015a346b6eb9b
Lam <daibanglam@gmail.com>
2023-07-13 12:49:18 +0700

    feat: handling data fetching error

--- src/user/UserService.ts
+++ src/user/UserService.ts
@@ -3,22 +3,16 @@ import { PromiseCache } from 'src/common/PromiseCache';
 export class UserService {
   constructor(private readonly cacheService: PromiseCache) {}
 
-  public getAlbum(albumId: string): Promise<string> {
-    return this.cacheService.get(
-      `albums/${albumId}`,
-      () =>
-        new Promise<string>((rs, rj) => {
-          setTimeout(() => rj(albumId), 1000);
-        })
-    );
-  }
-
   public getProfile(): Promise<string> {
     return this.cacheService.get(
       'profile',
       () =>
-        new Promise<string>((rs) => {
-          setTimeout(() => rs('my profile'), 1000);
+        new Promise<string>((rs, rj) => {
+          setTimeout(
+            () =>
+              Math.random() >= 0.5 ? rs('Loaded Profile') : rj(new Error('Fetch data failed')),
+            1500
+          );
         })
     );
   }
