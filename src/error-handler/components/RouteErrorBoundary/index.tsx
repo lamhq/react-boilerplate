47d7f710f063a24f97bfd7fd2df5cba8ce9107ee
Lam <daibanglam@gmail.com>
2023-07-21 10:38:04 +0700

    chore: add comment for clarification

--- src/error-handler/components/RouteErrorBoundary/index.tsx
+++ src/error-handler/components/RouteErrorBoundary/index.tsx
@@ -2,6 +2,8 @@ import { useRouteError } from 'react-router-dom';
 
 export default function RouteErrorBoundary() {
   const error = useRouteError();
+  // throw the error so it will be handled by parent error boundary
   throw error;
+
   return <p>Not rendered</p>;
 }
