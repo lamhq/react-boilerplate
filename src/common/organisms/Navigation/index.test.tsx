44d9cca2e42740299f519acb083153d1f7b81843
Lam <daibanglam@gmail.com>
2023-08-05 09:52:01 +0700

    feat: update navigation menu

---
+++ src/common/organisms/Navigation/index.test.tsx
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import Navigation from '.';
+
+describe('Navigation', () => {
+  const router = createMemoryRouter(
+    [
+      {
+        path: '*',
+        element: <Navigation />,
+      },
+    ],
+    {
+      initialEntries: ['/', '/home'],
+      initialIndex: 1, // start at "/home"
+    }
+  );
+
+  it('should renders home navigation item correctly', () => {
+    const { getByText } = render(<RouterProvider router={router} />);
+    const homeNavItem = getByText('Home');
+    expect(homeNavItem).toBeInTheDocument();
+  });
+
+  it('renders lazy loading navigation items correctly', () => {
+    const { getByText } = render(<RouterProvider router={router} />);
+    const intersectionApiNavItem = getByText('Intersection API');
+    const lazyAttributeNavItem = getByText('Lazy Attribute');
+    expect(intersectionApiNavItem).toBeInTheDocument();
+    expect(lazyAttributeNavItem).toBeInTheDocument();
+  });
+});
