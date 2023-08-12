0e79a500f2151b908902f95db05cbf00496f0156
Lam <daibanglam@gmail.com>
2023-08-12 10:43:26 +0700

    docs: update doc theme config

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <span>React Boilerplate Documentation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra'
+    link: 'https://github.com/lamhq/react-boilerplate'
   }
   // ...
 }
